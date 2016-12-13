c82bdda789419a8ec24347aa438b1002f8316667
lilott8 <lilott8@gmail.com>
2016-12-12 17:02:14 -0800

    login creds

--- django_sec/management/commands/sec_import_index.py
+++ django_sec/management/commands/sec_import_index.py
@@ -113,7 +113,7 @@ class Command(BaseCommand):
         if not os.path.exists(fn):
             print('Downloading %s.' % (url,))
 
-            ftp = FTP('ftp.sec.gov')
+            ftp = FTP('ftp.sec.gov', user="anonymous", passwd="anonymous")
 
             ftp.retrbinary('RETR %s' % path, open(fn, 'wb').write)
             #urllib.request.urlop

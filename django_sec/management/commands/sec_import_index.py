235a458e3e41af95098d893fe429a78833005fec
lilott8 <lilott8@gmail.com>
2016-12-12 16:21:33 -0800

    fixed more noargs

--- django_sec/management/commands/sec_import_index.py
+++ django_sec/management/commands/sec_import_index.py
@@ -25,12 +25,12 @@ class Command(BaseCommand):
     help = "Download new files representing one month of 990s, ignoring months we already have. "\
         "Each quarter contains hundreds of thousands of filings; will take a while to run. "
     def add_arguments(self, parser):
-        parser.add_argument('start_year', type=int)
-        parser.add_argument('end_year', type=int)
-        parser.add_argument('quarter', type=int)
-        parser.add_argument('delete-prior-indexes', type=bool)
-        parser.add_argument('reprocess', type=bool)
-        parser.add_argument('auto-reprocess-last-n-days', type=int)
+        parser.add_argument('start_year', type=int, default=None)
+        parser.add_argument('end_year', type=int, default=None)
+        parser.add_argument('quarter', type=int, default=None)
+        parser.add_argument('delete-prior-indexes', type=bool, action='store_true', default=False)
+        parser.add_argument('reprocess', type=bool, action='store_true', default=False)
+        parser.add_argument('auto-reprocess-last-n-days', type=int, help='The number of days to automatically redownload and reprocess index files.', default=90)
 
     #args = ''
     """
